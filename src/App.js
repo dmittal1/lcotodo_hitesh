55464b5ebfa0df3dd97a19f4d705163ddc44b6a0
`Dinesh <`mittald@hotmail.co.uk>
2020-01-19 23:27:02 +0000

    Changes Made

--- src/App.js
+++ src/App.js
@@ -3,91 +3,30 @@ import logo from "./LCO-logo-white.png";
 import "./App.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newItem: "",
-      list: []
-    };
-  }
 
-  addItem(todoValue) {
-    if (todoValue !== "") {
-      const newItem = {
-        id: Date.now(),
-        value: todoValue,
-        isDone: false
-      };
-      const list = [...this.state.list];
-      list.push(newItem);
-
-      this.setState({
-        list,
-        newItem: ""
-      });
-    }
-  }
-
-  deleteItem(id) {
-    const list = [...this.state.list];
-    const updatedlist = list.filter(item => item.id !== id);
-    this.setState({ list: updatedlist });
-  }
-
-  updateInput(input) {
-    this.setState({ newItem: input });
-  }
-
-  render() {
-    return (
+  render(){
+    return(
       <div>
-        <img src={logo} width="100" height="100" className="logo" />
+        <img src={logo} width="100" height="100" className="logo"/>
         <h1 className="app-title">LCO ToDo App</h1>
         <div className="container">
-          Add an Item....
+          Add an Item...
           <br />
-          <input
-            type="text"
-            className="input-text"
-            placeholder="Write a Todo"
-            required
-            value={this.state.newItem}
-            onChange={e => this.updateInput(e.target.value)}
+          <input 
+          type="text" 
+          className="input-text" 
+          placeholder="Write a Todo"
           />
-          <button
-            className="add-btn"
-            onClick={() => this.addItem(this.state.newItem)}
-            disabled={!this.state.newItem.length}
-          >
-            Add Todo
-          </button>
+          <button className="add-btn">Add Todo</button>
           <div className="list">
             <ul>
-              {this.state.list.map(item => {
-                return (
-                  <li key={item.id}>
-                    <input
-                      type="checkbox"
-                      name="idDone"
-                      checked={item.isDone}
-                      onChange={() => {}}
-                    />
-                    {item.value}
-                    <button
-                      className="btn"
-                      onClick={() => this.deleteItem(item.id)}
-                    >
-                      Delete
-                    </button>
-                  </li>
-                );
-              })}
               <li>
                 <input type="checkbox" name="" id="" />
                 Record youtube videos
                 <button className="btn">Delete</button>
               </li>
             </ul>
+
           </div>
         </div>
       </div>
@@ -96,3 +35,16 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
